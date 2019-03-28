22cc77cfcb8f4d178404e0c280c8a80e4c948d9f
EdsonCandido <edson.c.a.j@hotmail.com>
2019-03-28 00:29:49 -0300

    Adicionada a Rota que Lista Todos os Usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../bd');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+  Customer.find({}).lean().exec((e,docs) => {
+    res.json(docs);
+    res.end();
+  });
 });
 
 module.exports = router;
